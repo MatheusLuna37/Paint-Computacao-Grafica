a18bdc33674fc313901485b99aeae949ed9b4d60
IrlanBarros <irlanbarros100@gmail.com>
2025-08-22 21:58:54 -0300

    add: implement display function and header file

---
+++ src/eventos/display.c
@@ -0,0 +1,26 @@
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "../../cabecalhos/display.h"
+#include "../../cabecalhos/globais.h"
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(5);
+
+    glColor3f(0.0, 0.0, 0.0); // Preto
+    glBegin(GL_LINES);
+    glEnd();
+
+    desenhar_pontos(pontos);
+    desenhar_linhas(linhas);
+    desenhar_poligonos(poligonos);
+
+    if (desenhar_previa_linha(mouseX, mouseY));
+    if (desenhar_previa_poligono(mouseX, mouseY));
+
+    glFlush();
+}
