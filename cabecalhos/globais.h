c7053579f59f3b74f841eaa1d89a63191d9919fc
IrlanBarros <irlanbarros100@gmail.com>
2025-08-22 21:59:49 -0300

    add: include additional headers and define global variables in globais.h and globais.c

--- cabecalhos/globais.h
+++ cabecalhos/globais.h
@@ -1,6 +1,10 @@
 #ifndef GLOBAIS_H_INCLUDED
 #define GLOBAIS_H_INCLUDED
 
+#include "pontos.h"
+#include "linhas.h"
+#include "poligonos.h"
+
 extern float **TRANSLACAO;
 extern float **ROTACAO;
 extern float **ESCALA;
@@ -11,6 +15,18 @@ extern float **CISALHAMENTO_CENTROIDE;
 extern float **PONTO_HOMOGENEO;
 extern float **RESULTADO;
 extern float **RESULTADOP;
+extern int objeto;
+extern int modo;
+extern int action;
+extern Pontos *pontos;
+extern Linhas *linhas;
+extern Poligonos *poligonos;
+extern float mouseX, mouseY;
+extern int esquerdo;
+extern int direito;
+extern int dir;
+extern Pontos SELECIONADO;
+extern int screenWidth, screenHeight;
 
 extern int screenWidth;
 extern int screenHeight;
