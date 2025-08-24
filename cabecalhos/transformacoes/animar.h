5a0d294f1ad05f7e8a6bb6b3048e55bc5074d654
IrlanBarros <irlanbarros100@gmail.com>
2025-08-23 23:17:54 -0300

    add: implement animation functions and collision detection in animar.c and animar.h

---
+++ cabecalhos/transformacoes/animar.h
@@ -0,0 +1,49 @@
+#ifndef ANIMAR_H
+#define ANIMAR_H
+
+#include <stdbool.h>
+#include "../estruturas/pontos.h"
+#include "../estruturas/linhas.h"
+#include "../estruturas/poligonos.h"
+
+void reiniciar_animacao();
+void excluir_tudo();
+void calcular_bounding_box
+    (
+        Pontos pts, 
+        float *minX, 
+        float *maxX, 
+        float *minY, 
+        float *maxY
+    );
+bool checar_colisao_poligonos
+    (
+        Poligonos p, 
+        float minX, 
+        float maxX, 
+        float minY, 
+        float maxY
+    );
+bool comer_ponto
+    (
+        float minX, 
+        float maxX, 
+        float minY, 
+        float maxY, 
+        Poligonos p
+    );
+bool comer_linha
+    (
+        float minX, 
+        float maxX, 
+        float minY, 
+        float maxY, 
+        Poligonos p
+    );
+void desenhar_game_over();
+bool colisao_bbox(float minA, float maxA, float minB, float maxB);
+void iniciar_animacao();
+void parar_animacao();
+void animar(int value);
+
+#endif
